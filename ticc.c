bc8829b1ef11a40d2d7c9bec7d017b364e63229f
robo-monk <rrobomonk@gmail.com>
2023-01-23 22:50:22 +0200

    turns

--- ticc.c
+++ ticc.c
@@ -5,13 +5,21 @@ const BOXES_LEN = 9;
 enum BoxState {
   EMPTY = 0,
   X = 1,
-  Y = 2
+  O = 2
 };
+
 
 struct GameState {
   int turn;
   enum BoxState boxes[BOXES_LEN];
 };
+
+enum BoxState derive_next_box_state(struct GameState *game) {
+  printf("checking turn for game (i: %d) \n\n", game->turn);
+  enum BoxState next_box_state = (game->turn) % 2 == 0 ? X : O;
+
+  return next_box_state;
+}
 
 struct GameState init_game() {
 
@@ -26,11 +34,20 @@ struct GameState init_game() {
   return game;
 }
 
+void play(struct GameState *game, int index) {
+  enum BoxState new_box = derive_next_box_state(game);
+  printf("placing at %d (turn: %c) \n\n", index, new_box == 1 ? 'x' : 'o');
+
+  game->turn += 1;
+}
 
 int main() {
-  printf("starting tic tac toe super engine");
+  printf("starting tic tac toe super engine \n\n");
 
   struct GameState game = init_game();
+
+  play(&game, 3);
+  play(&game, 4);
 
   return 0;
 }
